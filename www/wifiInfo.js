b642ff85741775d1cb77b5adb253526503fe4d87
casephonegap <office@case.at>
2015-03-18 15:00:47 +0100

    Update wifiInfo.js

--- www/wifiInfo.js
+++ www/wifiInfo.js
@@ -2,8 +2,8 @@ function wifiInfo() { };
 
 /*  wifiInfo*/
 wifiInfo.prototype.getConnectionInfo = function (p_callback) 
-{ console.log('wifiInfo aufgerufen: ' + p_callback);
-  cordova.exec(function(obj) { console.log('callback aufrufen: ' + obj); p_callback(obj); }, null, 'wifiInfo', 'getConnectionInfo', []);
+{ //console.log('wifiInfo aufgerufen: ' + p_callback);
+  cordova.exec(p_callback, null, 'wifiInfo', 'getConnectionInfo', []);
 };
 
 module.exports = new wifiInfo();
